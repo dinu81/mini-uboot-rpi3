795533c7a69d2a01828fd7d3ddae6515fee13099
dineshkumarbhaskaran <dineshkumarb@gmail.com>
2016-11-18 14:00:02 +0530

    cleaned up board/functions

--- common/board_f.c
+++ common/board_f.c
@@ -19,29 +19,10 @@
 #include <console.h>
 #include <environment.h>
 #include <dm.h>
-#include <fdtdec.h>
-#include <fs.h>
-#if defined(CONFIG_CMD_IDE)
-#include <ide.h>
-#endif
 #include <initcall.h>
 #include <logbuff.h>
 #include <malloc.h>
 #include <mapmem.h>
-
-/* TODO: Can we move these into arch/ headers? */
-#ifdef CONFIG_8xx
-#include <mpc8xx.h>
-#endif
-#ifdef CONFIG_5xx
-#include <mpc5xx.h>
-#endif
-#ifdef CONFIG_MPC5xxx
-#include <mpc5xxx.h>
-#endif
-#if defined(CONFIG_MP) && (defined(CONFIG_MPC86xx) || defined(CONFIG_E500))
-#include <asm/mp.h>
-#endif
 
 #include <os.h>
 #include <post.h>
@@ -54,15 +35,6 @@
 #include <linux/errno.h>
 #include <asm/io.h>
 #include <asm/sections.h>
-#if defined(CONFIG_X86) || defined(CONFIG_ARC)
-#include <asm/init_helpers.h>
-#endif
-#if defined(CONFIG_X86) || defined(CONFIG_ARC) || defined(CONFIG_XTENSA)
-#include <asm/relocate.h>
-#endif
-#ifdef CONFIG_SANDBOX
-#include <asm/state.h>
-#endif
 #include <dm/root.h>
 #include <linux/compiler.h>
 
@@ -71,40 +43,7 @@
  *
  * Here we initialize it if needed.
  */
-#ifdef XTRN_DECLARE_GLOBAL_DATA_PTR
-#undef	XTRN_DECLARE_GLOBAL_DATA_PTR
-#define XTRN_DECLARE_GLOBAL_DATA_PTR	/* empty = allocate here */
-DECLARE_GLOBAL_DATA_PTR = (gd_t *) (CONFIG_SYS_INIT_GD_ADDR);
-#else
 DECLARE_GLOBAL_DATA_PTR;
-#endif
-
-/*
- * TODO(sjg@chromium.org): IMO this code should be
- * refactored to a single function, something like:
- *
- * void led_set_state(enum led_colour_t colour, int on);
- */
-/************************************************************************
- * Coloured LED functionality
- ************************************************************************
- * May be supplied by boards if desired
- */
-__weak void coloured_LED_init(void) {}
-__weak void red_led_on(void) {}
-__weak void red_led_off(void) {}
-__weak void green_led_on(void) {}
-__weak void green_led_off(void) {}
-__weak void yellow_led_on(void) {}
-__weak void yellow_led_off(void) {}
-__weak void blue_led_on(void) {}
-__weak void blue_led_off(void) {}
-
-
-__weak void board_add_ram_info(int use_default)
-{
-	/* please define platform specific board_add_ram_info() */
-}
 
 static int display_text_info(void)
 {
@@ -113,11 +52,7 @@ static int display_text_info(void)
 	bss_start = (ulong)&__bss_start;
 	bss_end = (ulong)&__bss_end;
 
-#ifdef CONFIG_SYS_TEXT_BASE
 	text_base = CONFIG_SYS_TEXT_BASE;
-#else
-	text_base = CONFIG_SYS_MONITOR_BASE;
-#endif
 
 	debug("U-Boot code: %08lX -> %08lX  BSS: -> %08lX\n",
 		text_base, bss_start, bss_end);
@@ -134,9 +69,9 @@ static int announce_dram_init(void)
 
 static int show_dram_config(void)
 {
+#if 0
 	unsigned long long size;
 
-#ifdef CONFIG_NR_DRAM_BANKS
 	int i;
 
 	debug("\nRAM Configuration:\n");
@@ -149,23 +84,18 @@ static int show_dram_config(void)
 #endif
 	}
 	debug("\nDRAM:  ");
-#else
-	size = gd->ram_size;
-#endif
 
 	print_size(size, "");
 	board_add_ram_info(0);
 	putc('\n');
-
+#endif
 	return 0;
 }
 
 __weak void dram_init_banksize(void)
 {
-#if defined(CONFIG_NR_DRAM_BANKS) && defined(CONFIG_SYS_SDRAM_BASE)
 	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
 	gd->bd->bi_dram[0].size = get_effective_memsize();
-#endif
 }
 
 __maybe_unused
@@ -182,19 +112,9 @@ static int setup_mon_len(void)
 	return 0;
 }
 
-__weak int arch_cpu_init(void)
-{
-	return 0;
-}
-
-__weak int mach_cpu_init(void)
-{
-	return 0;
-}
 /* Get the top of usable RAM */
 __weak ulong board_get_usable_ram_top(ulong total_size)
 {
-#ifdef CONFIG_SYS_SDRAM_BASE
 	/*
 	 * Detect whether we have so much RAM that it goes past the end of our
 	 * 32-bit address space. If so, clip the usable RAM so it doesn't.
@@ -205,17 +125,13 @@ __weak ulong board_get_usable_ram_top(ulong total_size)
 		 * are made.
 		 */
 		return 0;
-#endif
+
 	return gd->ram_top;
 }
 
 __weak phys_size_t board_reserve_ram_top(phys_size_t ram_size)
 {
-#ifdef CONFIG_SYS_MEM_TOP_HIDE
-	return ram_size - CONFIG_SYS_MEM_TOP_HIDE;
-#else
 	return ram_size;
-#endif
 }
 
 static int setup_dest_addr(void)
@@ -225,15 +141,7 @@ static int setup_dest_addr(void)
 	 * Ram is setup, size stored in gd !!
 	 */
 	debug("Ram size: %08lX\n", (ulong)gd->ram_size);
-#ifdef CONFIG_SYS_MEM_RESERVE_SECURE
-	/* Reserve memory for secure MMU tables, and/or security monitor */
-	gd->ram_size -= CONFIG_SYS_MEM_RESERVE_SECURE;
-	/*
-	 * Record secure memory location. Need recalcuate if memory splits
-	 * into banks, or the ram base is not zero.
-	 */
-	gd->arch.secure_ram = gd->ram_size;
-#endif
+
 	/*
 	 * Subtract specified amount of memory to hide so that it won't
 	 * get "touched" at all by U-Boot. By fixing up gd->ram_size
@@ -245,18 +153,13 @@ static int setup_dest_addr(void)
 	 */
 	gd->ram_size = board_reserve_ram_top(gd->ram_size);
 
-#ifdef CONFIG_SYS_SDRAM_BASE
 	gd->ram_top = CONFIG_SYS_SDRAM_BASE;
-#endif
 	gd->ram_top += get_effective_memsize();
 	gd->ram_top = board_get_usable_ram_top(gd->mon_len);
 	gd->relocaddr = gd->ram_top;
 	debug("Ram top: %08lX\n", (ulong)gd->ram_top);
 	return 0;
 }
-
-
-
 
 /* Round memory pointer down to next 4 kB limit */
 static int reserve_round_4k(void)
@@ -265,8 +168,6 @@ static int reserve_round_4k(void)
 	return 0;
 }
 
-#if !(defined(CONFIG_SYS_ICACHE_OFF) && defined(CONFIG_SYS_DCACHE_OFF)) && \
-		defined(CONFIG_ARM)
 static int reserve_mmu(void)
 {
 	/* reserve TLB table */
@@ -282,27 +183,12 @@ static int reserve_mmu(void)
 
 	return 0;
 }
-#endif
 
 
-# ifdef CONFIG_LCD
 static int reserve_lcd(void)
 {
-#  ifdef CONFIG_FB_ADDR
 	gd->fb_base = CONFIG_FB_ADDR;
-#  else
-	/* reserve memory for LCD display (always full pages) */
-	gd->relocaddr = lcd_setmem(gd->relocaddr);
-	gd->fb_base = gd->relocaddr;
-#  endif /* CONFIG_FB_ADDR */
 
-	return 0;
-}
-# endif /* CONFIG_LCD */
-
-
-static int reserve_trace(void)
-{
 	return 0;
 }
 
@@ -323,7 +209,6 @@ static int reserve_uboot(void)
 	return 0;
 }
 
-#ifndef CONFIG_SPL_BUILD
 /* reserve memory for malloc() area */
 static int reserve_malloc(void)
 {
@@ -345,15 +230,6 @@ static int reserve_board(void)
 	}
 	return 0;
 }
-#endif
-
-static int setup_machine(void)
-{
-#ifdef CONFIG_MACH_TYPE
-	gd->bd->bi_arch_number = CONFIG_MACH_TYPE; /* board id for Linux */
-#endif
-	return 0;
-}
 
 static int reserve_global_data(void)
 {
@@ -361,32 +237,6 @@ static int reserve_global_data(void)
 	gd->new_gd = (gd_t *)map_sysmem(gd->start_addr_sp, sizeof(gd_t));
 	debug("Reserving %zu Bytes for Global Data at: %08lx\n",
 			sizeof(gd_t), gd->start_addr_sp);
-	return 0;
-}
-
-static int reserve_fdt(void)
-{
-#ifndef CONFIG_OF_EMBED
-	/*
-	 * If the device tree is sitting immediately above our image then we
-	 * must relocate it. If it is embedded in the data section, then it
-	 * will be relocated with other data.
-	 */
-	if (gd->fdt_blob) {
-		gd->fdt_size = ALIGN(fdt_totalsize(gd->fdt_blob) + 0x1000, 32);
-
-		gd->start_addr_sp -= gd->fdt_size;
-		gd->new_fdt = map_sysmem(gd->start_addr_sp, gd->fdt_size);
-		debug("Reserving %lu Bytes for FDT at: %08lx\n",
-		      gd->fdt_size, gd->start_addr_sp);
-	}
-#endif
-
-	return 0;
-}
-
-int arch_reserve_stacks(void)
-{
 	return 0;
 }
 
@@ -400,35 +250,13 @@ static int reserve_stacks(void)
 	 * let the architecture-specific code tailor gd->start_addr_sp and
 	 * gd->irq_sp
 	 */
-	return arch_reserve_stacks();
-}
-
-static int display_new_sp(void)
-{
-	debug("New Stack Pointer is: %08lx\n", gd->start_addr_sp);
-
 	return 0;
 }
-
 
 static int setup_dram_config(void)
 {
 	/* Ram is board specific, so move it to board code ... */
 	dram_init_banksize();
-
-	return 0;
-}
-
-static int reloc_fdt(void)
-{
-#ifndef CONFIG_OF_EMBED
-	if (gd->flags & GD_FLG_SKIP_RELOC)
-		return 0;
-	if (gd->new_fdt) {
-		memcpy(gd->new_fdt, gd->fdt_blob, gd->fdt_size);
-		gd->fdt_blob = gd->new_fdt;
-	}
-#endif
 
 	return 0;
 }
@@ -440,9 +268,7 @@ static int setup_reloc(void)
 		return 0;
 	}
 
-#ifdef CONFIG_SYS_TEXT_BASE
 	gd->reloc_off = gd->relocaddr - CONFIG_SYS_TEXT_BASE;
-#endif
 	memcpy(gd->new_gd, (char *)gd, sizeof(gd_t));
 
 	debug("Relocation Offset is: %08lx\n", gd->reloc_off);
@@ -453,7 +279,6 @@ static int setup_reloc(void)
 	return 0;
 }
 
-
 /* Record the board_init_f() bootstage (after arch_cpu_init()) */
 static int mark_bootstage(void)
 {
@@ -462,56 +287,25 @@ static int mark_bootstage(void)
 	return 0;
 }
 
-static int initf_console_record(void)
-{
-	return 0;
-}
-
 static int initf_dm(void)
 {
-#if defined(CONFIG_DM) && defined(CONFIG_SYS_MALLOC_F_LEN)
 	int ret;
 
 	ret = dm_init_and_scan(true);
 	if (ret)
 		return ret;
-#endif
-	return 0;
-}
 
-/* Architecture-specific memory reservation */
-__weak int reserve_arch(void)
-{
-	return 0;
-}
-
-__weak int arch_cpu_init_dm(void)
-{
 	return 0;
 }
 
 static init_fnc_t init_sequence_f[] = {
 	setup_mon_len,
 	initf_malloc,
-	initf_console_record,
-	arch_cpu_init,		/* basic arch cpu dependent setup */
-	mach_cpu_init,		/* SoC/machine dependent CPU setup */
 	initf_dm,
-	arch_cpu_init_dm,
 	mark_bootstage,		/* need timer, go after init dm */
-#if defined(CONFIG_BOARD_EARLY_INIT_F)
 	board_early_init_f,
-#endif
 	/* TODO: can any of this go into arch_cpu_init()? */
-#if defined(CONFIG_ARM) || defined(CONFIG_MIPS) || \
-		defined(CONFIG_BLACKFIN) || defined(CONFIG_NDS32) || \
-		defined(CONFIG_SPARC)
 	timer_init,		/* initialize timer */
-#endif
-	env_init,		/* initialize environment */
-#if 0
-	init_baud_rate,		/* initialze baudrate settings */
-#endif 
 	serial_init,		/* serial communications setup */
 	console_init_f,		/* stage 1 init of console */
 	display_options,	/* say that we are here */
@@ -519,35 +313,19 @@ static init_fnc_t init_sequence_f[] = {
 	print_cpuinfo,		/* display cpu info (and speed) */
 	announce_dram_init,
 	/* TODO: unify all these dram functions? */
-#if defined(CONFIG_ARM) || defined(CONFIG_X86) || defined(CONFIG_NDS32) || \
-		defined(CONFIG_MICROBLAZE) || defined(CONFIG_AVR32)
 	dram_init,		/* configure available RAM banks */
-#endif
 	setup_dest_addr,
 	reserve_round_4k,
-#if !(defined(CONFIG_SYS_ICACHE_OFF) && defined(CONFIG_SYS_DCACHE_OFF)) && \
-		defined(CONFIG_ARM)
 	reserve_mmu,
-#endif
 	reserve_lcd,
-	reserve_trace,
-#if !defined(CONFIG_BLACKFIN) && !defined(CONFIG_XTENSA)
 	reserve_uboot,
-#endif
-#ifndef CONFIG_SPL_BUILD
 	reserve_malloc,
 	reserve_board,
-#endif
-	setup_machine,
 	reserve_global_data,
-	reserve_fdt,
-	reserve_arch,
 	reserve_stacks,
 	setup_dram_config,
 	show_dram_config,
-	display_new_sp,
 	INIT_FUNC_WATCHDOG_RESET
-	reloc_fdt,
 	setup_reloc,
 	NULL,
 };
--- common/board_r.c
+++ common/board_r.c
@@ -11,74 +11,28 @@
  */
 
 #include <common.h>
-/* TODO: can we just include all these headers whether needed or not? */
-#if defined(CONFIG_CMD_BEDBUG)
-#include <bedbug/type.h>
-#endif
 #include <command.h>
 #include <console.h>
-#ifdef CONFIG_HAS_DATAFLASH
-#include <dataflash.h>
-#endif
 #include <dm.h>
 #include <environment.h>
 #include <fdtdec.h>
-#if defined(CONFIG_CMD_IDE)
-#include <ide.h>
-#endif
 #include <initcall.h>
-#ifdef CONFIG_PS2KBD
-#include <keyboard.h>
-#endif
-#if defined(CONFIG_CMD_KGDB)
-#include <kgdb.h>
-#endif
 #include <logbuff.h>
 #include <malloc.h>
 #include <mapmem.h>
-#ifdef CONFIG_BITBANGMII
-#include <miiphy.h>
-#endif
 #include <stdio_dev.h>
-#ifdef CONFIG_CMD_AMBAPP
-#include <ambapp.h>
-#endif
-#ifdef CONFIG_ADDR_MAP
-#include <asm/mmu.h>
-#endif
 #include <asm/sections.h>
-#ifdef CONFIG_X86
-#include <asm/init_helpers.h>
-#endif
 #include <dm/root.h>
 #include <linux/compiler.h>
 #include <linux/err.h>
-#ifdef CONFIG_AVR32
-#include <asm/arch/mmu.h>
-#endif
 #include <efi_loader.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#if defined(CONFIG_SPARC)
-extern int prom_init(void);
-#endif
-
 ulong monitor_flash_len;
-
-__weak int board_flash_wp_on(void)
-{
-	/*
-	 * Most flashes can't be detected when write protection is enabled,
-	 * so provide a way to let U-Boot gracefully ignore write protected
-	 * devices.
-	 */
-	return 0;
-}
 
 static int initr_trace(void)
 {
-
 	return 0;
 }
 
@@ -90,29 +44,16 @@ static int initr_reloc(void)
 	return 0;
 }
 
-#ifdef CONFIG_ARM
-/*
- * Some of these functions are needed purely because the functions they
- * call return void. If we change them to return 0, these stubs can go away.
- */
 static int initr_caches(void)
 {
 	/* Enable caches */
 	enable_caches();
 	return 0;
 }
-#endif
-
-__weak int fixup_cpu(void)
-{
-	return 0;
-}
 
 static int initr_reloc_global_data(void)
 {
-#ifdef __ARM__
 	monitor_flash_len = _end - __image_copy_start;
-#endif
 
 	return 0;
 }
@@ -126,33 +67,15 @@ static int initr_malloc(void)
 {
 	ulong malloc_start;
 
-#ifdef CONFIG_SYS_MALLOC_F_LEN
 	debug("Pre-reloc malloc() used %#lx bytes (%ld KB)\n", gd->malloc_ptr,
 	      gd->malloc_ptr / 1024);
-#endif
+
 	/* The malloc area is immediately below the monitor copy in DRAM */
 	malloc_start = gd->relocaddr - TOTAL_MALLOC_LEN;
 	mem_malloc_init((ulong)map_sysmem(malloc_start, TOTAL_MALLOC_LEN),
 			TOTAL_MALLOC_LEN);
 	return 0;
 }
-
-#ifdef CONFIG_SYS_NONCACHED_MEMORY
-static int initr_noncached(void)
-{
-	noncached_init();
-	return 0;
-}
-#endif
-
-#ifdef CONFIG_GENERIC_MMC
-static int initr_mmc(void)
-{
-	puts("MMC:   ");
-	mmc_initialize(gd->bd);
-	return 0;
-}
-#endif
 
 static int initr_env(void)
 {
@@ -179,16 +102,10 @@ static int run_main_loop(void)
 init_fnc_t init_sequence_r[] = {
 	initr_trace,
 	initr_reloc,
-#ifdef CONFIG_ARM
 	initr_caches,
-#endif
 	initr_reloc_global_data,
 	initr_barrier,
 	initr_malloc,
-#ifdef CONFIG_GENERIC_MMC
-    /* Dinesh: without this blank screen */
-	initr_mmc,
-#endif
 	initr_env,
     /* Dinesh without this rainbow */
 	stdio_add_devices,
